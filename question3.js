fafeb6b43df7940b05ea28ff429b967bfac14b1c
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-06 23:27:45 +0530

    rotateArray assignment

--- question3.js
+++ question3.js
@@ -1,13 +1,12 @@
 const nums = [1, 2, 3, 4, 5, 6, 7];
-let k = 3;
+;
 
 function rotateArray(nums, k) {
-  k = k % nums.length;
-  const removed = nums.splice(nums.length - k, k);
-  nums.unshift(...removed);
+  const newNum = nums.splice(nums.length - k, k);
+  nums.unshift(...newNum);
   console.log(nums);
 }
-rotateArray(nums, k);
+rotateArray(nums, k=3);
 
 const nums2 = [-1, -100, 3, 99];
 rotateArray(nums2, (k = 2));
